fbb931fe7850f9ac4bbbd61b94d9fea64d720a05
KAMORU, Lateef Gbolahan <kamorulateef@hotmail.com>
2023-06-14 15:26:50 +0100

    Update useroutput.jsx

--- src/components/useroutput.jsx
+++ src/components/useroutput.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
-function useroutput() {
+function useroutput(props) {
   return (
     <div className='useroutput'>
-        <h1><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi assumenda eveniet aliquam eligendi, et vero id eos, fuga doloremque tenetur esse molestias tempore cupiditate expedita quae, at enim aliquid. Laudantium.</p>
- <br />
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, qui. Maxime libero fuga, commodi cupiditate fugit blanditiis sunt ducimus veniam maiores? Voluptas cum numquam ad praesentium mollitia. A, nulla porro!</p></h1>
+      <h1>My name is {props.username}, I live in {props.address}.</h1>
+ <br/>
     </div>
   )
 }
