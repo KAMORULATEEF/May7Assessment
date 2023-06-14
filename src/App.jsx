192481c1573a43ac2d5ddd7c3707ec06bfb8a51e
KAMORU, Lateef Gbolahan <kamorulateef@hotmail.com>
2023-06-14 15:25:26 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css'
-import React from 'react'
+import React, { useState } from 'react'
 import Useroutput from './components/useroutput'
 import Userinput from './components/userinput'
 
 function App() {
+  const [username, setUsername]= useState("Ayileru")
+  function changeName(e) {
+    setUsername(e.target.value)
+  }
   return (
     <div className='container'>
-      <Useroutput/>
-      <Userinput/>
+      <Useroutput username = {username} address = "Iyana Isashi"/>
+      <Userinput change={changeName}/>
     </div>
   )
 }
 
 export default App
+ 
