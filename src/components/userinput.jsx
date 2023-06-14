ddbdf63d5f263ceb8b7da6428afbc6d49ef2cea5
KAMORU, Lateef Gbolahan <kamorulateef@hotmail.com>
2023-06-14 15:26:16 +0100

    Update userinput.jsx

--- src/components/userinput.jsx
+++ src/components/userinput.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-function userinput() {
+
+function userinput(props) {
   return (
       <div 
-      className='userinput'><input type="text" className='input_txt' placeholder='enter text here'/></div>
+      className='userinput'><input type="text" className='input_txt' onChange={props.change} value={props.value} placeholder='enter text here'/></div>
   )
 }
 
 export default userinput
+
